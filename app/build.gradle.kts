baf992bbb03cc3c53e33f8413344f08e9486c433
Rofaida <rofaidasobhy123@gmail.com>
2025-03-28 00:45:17 +0200

    HomeScreen: UI(WeatherItemCard)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation ("com.github.bumptech.glide:compose:1.0.0-beta01")
+
 }
